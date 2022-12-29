b369f4ae0a3baa91cd4e4df6ff4f58f62f2a593a
0111-daisuke <s21C1127lm@s.chibakoudai.jp>
2022-12-29 09:43:24 +0900

    add a test

--- build/mypkg/build/lib/mypkg/talker.py
+++ build/mypkg/build/lib/mypkg/talker.py
@@ -1,18 +1,24 @@
 import rclpy
 from rclpy.node import Node
-from person_msgs.msg import Person
+from person_msgs.msg import Int16
 
-rclpy.init()
-node = Node("talker")
-pub = node.create_publisher(Person, "person", 10)
-n = 0
-def cb():
-    global n
-    msg = Person()
-    msg.name = "daisuke"
-    msg.age = n
-    pub.publish(msg)
-    n += 1
+class Talker():
+    def _init_(self):
+        self.pub = node.create_publisher(Int16, "countup", 10)
+        self.n = 0
+        node.create_timer(0.5, self.cb)
+        rclpy.init()
+        node = Node("talker")
+        talker = Talker
+
+        def cb(self):
+            msg = Int16()
+            msg.data = self.n
+            talker.pub.publish(msg)
+            talker.n += 1
 
 node.create_timer(0.5, cb)
+rclpy.init()
+node = Node("talker")
+talker = Talker(node)
 rclpy.spin(node)
