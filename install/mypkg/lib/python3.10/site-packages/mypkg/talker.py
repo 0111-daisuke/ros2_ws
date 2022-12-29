1b0023a6a4f88f805a61276138b9398d528c9a5e
0111-daisuke <s21C1127LM@s.chibakoudai.jp>
2022-12-29 14:57:43 +0900

    add test

--- install/mypkg/lib/python3.10/site-packages/mypkg/talker.py
+++ install/mypkg/lib/python3.10/site-packages/mypkg/talker.py
@@ -3,19 +3,19 @@ from rclpy.node import Node
 from person_msgs.msg import Int16
 
 class Talker():
-    def _init_(self):
+    def _init_(self, node):
         self.pub = node.create_publisher(Int16, "countup", 10)
         self.n = 0
         node.create_timer(0.5, self.cb)
         rclpy.init()
         node = Node("talker")
-        talker = Talker
+        talker = Talker()
 
         def cb(self):
             msg = Int16()
             msg.data = self.n
-            talker.pub.publish(msg)
-            talker.n += 1
+            self.pub.publish(msg)
+            self.n += 1
 
 node.create_timer(0.5, cb)
 rclpy.init()
